fba001e2f028407885afe5462e0a508c6382feb7
Raphael San Juan <raphaelsanjuan6@gmail.com>
2026-09-14 00:45:20 +0800

    created an animation and finish might be? the first page of the home

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,17 +1,57 @@
 "use client";
+
+import { SiFacebook, SiUpwork, SiGlassdoor, SiFreelancer } from '@icons-pack/react-simple-icons';
+import Button from "@/src/components/ui/button";
 import Image from "next/image";
+import usePageAnimation from '@/src/hooks/usePageAnimation';
+
+import gsap from 'gsap';
+import { useRef } from 'react';
 
 export default function Home(){
+    usePageAnimation();
+
+    const buttonRef = useRef(null);
+
+    const letsCollabClicked = () => {
+        gsap.fromTo(buttonRef.current, { scale: 0.95}, { scale: 1, duration: 1, ease: "back.out(2)"})
+    }
+
+    const mediaClicked = () => {
+
+    }
+
     return <main>
 
         <section className="mainPage w-full min-h-screen">
 
-            <div className="relative text-center self-end text-[5rem] items-center top-[170px] leading-18">
+            <div className="relative p-4">
+                <h1 className="webdev font-bold text-[2rem] items-center leading-18 tracking-tighter">WEB DEVELOPER</h1>
+                <p className="quote font-semibold text-[0.9rem]">
+                    I turn your ideas into modern, responsive, and functional websites designed to 
+                    bring your vision to life and help you achieve your goals.
+                </p>
+                <Button 
+                    className="letscollab border rounded-full w-[150px] h-auto text-xs mt-4 py-2 bg-black text-white font-semibold 
+                        cursor-pointer"
+                    onClick={letsCollabClicked}
+                    children="Let's collaborate ↗"
+                    ref={buttonRef}/>
+            </div>
+
+            <div className="name relative text-center self-end text-[5.5rem] items-center top-[25px] leading-18 tracking-tighter">
                 <h1 className="font-bungee-outline">RAPHA</h1>
                 <h1 className="font-bungee-inline">MHARC</h1>
             </div>
 
-            <Image src={"/images/my-image.png"} alt="my-image" width={1000} height={1000} className="absolute z-50 top-[365px] w-[1000px] h-auto"/>
+            <Image src={"/images/my-image.png"} alt="my-image" width={1000} height={1000} className="mypicture relative bottom-[50px] z-10 w-full h-auto"/>
+
+            <SiFacebook className="facebook relative bottom-[200px] left-[60px] rotate-300 active:scale-95 transition-all duration-300 ease-in-out"/>
+            <SiUpwork className="upwork relative bottom-[160px] left-[20px] rotate-20 active:scale-95 transition-all duration-300 ease-in-out"/>
+            <SiGlassdoor className="glassdoor relative bottom-[250px] left-[230px] rotate-10 active:scale-95 transition-all duration-300 ease-in-out"/>
+            <SiFreelancer className="freelancer relative bottom-[220px] left-[280px] rotate-10 active:scale-95 transition-all duration-300 ease-in-out"
+                onClick={mediaClicked}/>
+
         </section>
 
         <section className="projectsPage">
---
+++ src/hooks/usePageAnimation.tsx
@@ -0,0 +1,137 @@
+import { useGSAP } from "@gsap/react";
+import { ScrollTrigger } from "gsap/all";
+import gsap from "gsap";
+
+function homeAnimation(){
+    const tr = gsap.timeline();
+
+    tr.fromTo(".webdev", 
+        {
+            y: -40,
+            opacity: 0
+        },
+        {
+            y: 0,
+            opacity: 1,
+            duration: 1.,
+            ease: "power4.inOut"
+        },1
+    ).fromTo(".quote", 
+        {
+            y: -40,
+            opacity: 0
+        },
+        {
+            y: 0,
+            opacity: 1,
+            duration: 1.,
+            ease: "power4.inOut"
+        },1
+    ).fromTo(".letscollab", 
+        {
+            y: -40,
+            opacity: 0,
+        },
+        {
+            y: 0,
+            opacity: 1,
+            duration: 1,
+            ease: "power4.inOut"
+        },1
+    ).fromTo(".navbar", 
+        {
+            y: -40,
+            opacity: 0
+        },
+        {
+            y: 0,
+            opacity: 1,
+            duration: 1.,
+            ease: "power4.inOut"
+        },0
+    ).fromTo(".burger", 
+        {
+            y: -40,
+            opacity: 0
+        },
+        {
+            y: 0,
+            opacity: 1,
+            duration: 1.,
+            ease: "power4.inOut"
+        },0
+    ).fromTo(".name", 
+        {
+            scale: 0.2,
+            opacity: 0
+        },
+        {
+            scale: 1,
+            opacity: 1,
+            duration: 1.5,
+            ease: "power4.inOut"
+        },1
+    ).fromTo(".mypicture", 
+        {
+            y: 230,
+            filter: "blur(5px)"
+        },
+        {
+            y: 0,
+            filter: "blur(0px)",
+            duration: 1.6,
+            ease: "power4.in"
+        },1
+    ).fromTo(".facebook", 
+        {
+            opacity: 0,
+            filter: "blur(10px)"
+        },
+        {
+            opacity: 1,
+            filter: "blur(0px)",
+            duration: 1.4,
+            ease: "power2.in"
+        },1
+    ).fromTo(".glassdoor", 
+        {
+            opacity: 0,
+            filter: "blur(10px)"
+        },
+        {
+            opacity: 1.2,
+            filter: "blur(0px)",
+            duration: 1,
+            ease: "power2.in"
+        },1
+    ).fromTo(".freelancer", 
+        {
+            opacity: 0,
+            filter: "blur(10px)"
+        },
+        {
+            opacity: 1.1,
+            filter: "blur(0px)",
+            duration: 1,
+            ease: "power2.in"
+        },1
+    ).fromTo(".upwork", 
+        {
+            opacity: 0,
+            filter: "blur(10px)"
+        },
+        {
+            opacity: 1,
+            filter: "blur(0px)",
+            duration: 1,
+            ease: "power2.in"
+        },1
+    )
+    
+}
+
+export default function usePageAnimation(){
+    useGSAP(() => {
+        homeAnimation();
+    }, [])
+}
